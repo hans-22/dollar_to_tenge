59920f39884729aca6ce39b4e64f85f7cc8a4e88
prinnevald <prinnyprinnevald@gmail.com>
2019-08-30 04:31:35 +0600

    final version

--- dollar_to_tenge.py
+++ dollar_to_tenge.py
@@ -7,6 +7,15 @@ info = requests.get(source).text
 elements = BeautifulSoup(info, 'lxml').find('h1', {"style": "margin-top:5px;color:#ec1b1b;"})
 money = re.findall(r"[-+]?\d*\.\d+|\d+", elements.getText())
 
-userinput = float(input('Enter the amount of dollars for translating into tenge:\n'))
-print("USD -> KZT:")
-print(float(money[1]) * userinput)
+print("Currently 1 dollar equals to " + money[1] + " tenge")
+print("Select option [1] to translate USD -> KZT")
+print("Select option [2] to translate KZT -> USD")
+
+select = int(input())
+
+if(select == 1):
+	userinput = float(input('Enter the amount of USD for translating into KZT:\n'))
+	print(float(money[1]) * userinput)
+else:
+	userinput = float(input('Enter the amount of KZT to translate into USD:\n'))
+	print(userinput / float(money[1]))
